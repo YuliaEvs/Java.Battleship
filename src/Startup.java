30f39e98fb150ed4c304865e4c2cf3c37198be32
Yulia Evseeva <evseevayk@gmail.com>
2024-03-07 13:25:17 -0500

    first commit

---
+++ src/Startup.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class Startup {
+    private ArrayList<String> locationCells;
+    private String name;
+    public  void setLocationCells(ArrayList<String> loc) {
+
+        locationCells = loc;
+    }
+    public void setName(String n) {
+        name = n;
+    }
+    public String checkYourself(String userInput) {
+
+    }
+}
---
+++ src/StartupBust.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class StartupBust {
+    private GameHelper helper = new GameHelper();
+    private ArrayList<Startup> startups = new ArrayList<Startup>();
+    private int numOfGuesses = 0;
+
+    private void setUpGame() {
+        Starup one = new Startup();
+        one.setName("poniez");
+        Starup two = new Startup();
+        two.setName("hacqi");
+        Starup three = new Startup();
+        three.setName("cabista");
+        startips.add(one);
+        startips.add(two);
+        startips.add(three);
+    }
+}
